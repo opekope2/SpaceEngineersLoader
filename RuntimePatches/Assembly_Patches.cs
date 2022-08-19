e04fc5144079af561b3786f80317427bca3c32f3
opekope2 <opekope2@gmail.com>
2022-08-19 17:32:28 +0200

    Add patches to make SE launch at all

---
+++ RuntimePatches/Assembly_Patches.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace SpaceEngineers4Linux.RuntimePatches;
+
+[HarmonyPatch]
+public static class Assembly_Patches
+{
+    private static Assembly? entryAssembly;
+
+    [HarmonyPatch(typeof(Assembly), "GetEntryAssembly")]
+    [HarmonyPrefix]
+    public static bool GetEntryAssembly_Prefix(ref Assembly __result)
+    {
+        if (entryAssembly == null)
+        {
+            return true;
+        }
+
+        __result = entryAssembly;
+        return false;
+    }
+
+    public static void SetEntryAssembly(Assembly assembly)
+    {
+        entryAssembly = assembly;
+    }
+}
